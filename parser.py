a1798cdc76b72e90d659e5d606257bcb1ff59ed9
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-03-21 20:09:30 -0300

    parser done, no implementation tho

--- parser.py
+++ parser.py
@@ -3,20 +3,84 @@ import sys
 
 class Parser:
 
+    INCI_ARG_COUNT = 3
+
+
     def __init__(self, input_file):
         with open(input_file, 'r') as self.fin:
-            for i in range(7):
-                code = self.fin.readline().strip()
-                if code == '*COORDINATES':
+            for line in self.fin:
+                line = line.strip()
+                if line == '*COORDINATES':
                     self.read_coordinates()
+                elif line == '*INCIDENCES':
+                    self.read_incidences()
+                elif line == '*MATERIALS':
+                    self.read_materials()
+                elif line == '*GEOMETRIC_PROPERTIES':
+                    self.read_properties()
+                elif line == '*BCNODES':
+                    self.read_bcnodes()
+                elif line == '*LOADS':
+                    self.read_loads()
+
 
 
     def read_coordinates(self):
-        n = self.fin.readline().strip()
+        n = int(self.fin.readline().strip())
         self.coords = []
         for i in range(n):
-            line = fin.readline().strip().split()
-            self.coords[i][0], coords[i][1] = line[1], line[2]
+            line = self.fin.readline().strip().split()
+            tmp = [0, 0]
+            tmp[0], tmp[1] = float(line[1]), float(line[2])
+            self.coords.append(tmp)
+
+
+    def read_incidences(self):
+        self.inci = []
+        line = self.fin.readline().strip().split()
+        while len(line) == self.INCI_ARG_COUNT:
+            tmp = [0, 0]
+            tmp[0], tmp[1] = int(line[1]), int(line[2])
+            self.inci.append(tmp)
+            line = self.fin.readline().strip().split()
+
+
+    def read_materials(self):
+        self.materials = []
+        n = int(self.fin.readline().strip())
+        for i in range(n):
+            line = self.fin.readline().strip().split()
+            self.materials.append([float(line[0])])
+
+
+    def read_properties(self):
+        self.prop = []
+        n = int(self.fin.readline().strip())
+        for i in range(n):
+            line = self.fin.readline().strip().split()
+            self.prop.append([float(line[0])])
+
+
+    def read_bcnodes(self):
+        self.bcnodes = []
+        n = int(self.fin.readline().strip())
+        for i in range(n):
+            line = self.fin.readline().strip().split()
+            self.bcnodes.append([int(line[0]), int(line[1])])
+
+
+    def read_loads(self):
+        self.loads = []
+        n = int(self.fin.readline().strip())
+
+        # this can be optimized
+        for i in range(len(self.coords) * 2):
+            self.loads.append(0)
+
+        for i in range(n):
+            line = self.fin.readline().strip().split()
+            self.loads[(int(line[0]) - 1) * 2 + int(line[1]) - 1] = int(line[2])
+
 
 
 if __name__ == '__main__':
@@ -24,3 +88,8 @@ if __name__ == '__main__':
     parser = Parser(input_file)
 
     print(parser.coords)
+    print(parser.inci)
+    print(parser.materials)
+    print(parser.prop)
+    print(parser.bcnodes)
+    print(parser.loads)
